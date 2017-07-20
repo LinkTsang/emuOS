235ada6cdec52d58fc8b1dc55b72597b5257b77a
Link <linkdoyle@163.com>
2017-07-20 20:57:05 +0800

    add os files(1)

--- src/emuos/os/CentralProcessingUnit.java
+++ src/emuos/os/CentralProcessingUnit.java
@@ -3,169 +3,198 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package emuos.os
+package emuos.os;
 
-import java.util.BitSet
-import java.util.Timer
-import java.util.TimerTask
+import java.util.BitSet;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import emuos.generator.Instruction.*
+import static emuos.generator.Instruction.*;
 
 /**
  * @author Link
  */
-class CentralProcessingUnit {
-    private val memoryManager = MemoryManager()
-    private val processManager = ProcessManager(this, memoryManager)
-    private val timer = Timer(false)
-    /**
-     * @return the AX
-     */
-    var ax: Int = 0
-        private set
-    /**
-     * @return the PSW
-     */
-    var psw = BitSet(10)
-        private set
-    /**
-     * @return the IR
-     */
-    var ir: Int = 0
-        private set
-    /**
-     * @return the PC
-     */
-    var pc: Int = 0
-        private set
+public class CentralProcessingUnit {
+
+    private static final int PSW_INT_END = 9;
+    private static final int PSW_INT_TIME_SLICE = 8;
+    private static final int PSW_INT_IO = 7;
+    private static final int PSW_CF = 3;
+    private static final int PSW_SF = 2;
+    private static final int PSW_OF = 1;
+    private static final int PSW_ZF = 0;
+    private final MemoryManager memoryManager = new MemoryManager();
+    private final ProcessManager processManager = new ProcessManager(this, memoryManager);
+    private final Timer timer = new Timer(false);
+    private int AX;
+    private BitSet PSW = new BitSet(10);
+    private int IR;
+    private int PC;
+    private int time;
+    private int timeslice = 1;
+    private Listener onFinishedListener;
+
+    public static void main(String[] args) {
+        CentralProcessingUnit CPU = new CentralProcessingUnit();
+        CPU.setOnFinishedListener((cpu -> {
+            System.out.printf(" CPU stat (Current Time: %d)\n", cpu.getTime());
+            System.out.printf("TimeSlice = %d\n", cpu.getTimeSlice());
+            System.out.printf("AX = %d, PC = %d, PSW = %s\n", cpu.getAX(), cpu.getPC(), cpu.getPSW().toString());
+            System.out.println("----------");
+        }));
+        CPU.run();
+    }
+
+    public Listener setOnFinishedListener(Listener listener) {
+        Listener oldListener = onFinishedListener;
+        onFinishedListener = listener;
+        return oldListener;
+    }
+
+    public void CPU() {
+        time++;
+        if (PSW.get(PSW_INT_END)) {
+            interruptEnd();
+            PSW.clear(PSW_INT_END);
+        }
+        if (PSW.get(PSW_INT_TIME_SLICE)) {
+            interruptTime();
+            PSW.clear(PSW_INT_TIME_SLICE);
+        }
+        if (PSW.get(PSW_INT_IO)) {
+            interruptIO();
+            PSW.clear(PSW_INT_IO);
+        }
+
+        IR = nextByte();
+        execute();
+        if (--timeslice == 0) {
+            PSW.set(PSW_INT_TIME_SLICE);
+        }
+
+        if (onFinishedListener != null) {
+            onFinishedListener.on(this);
+        }
+    }
+
+    private byte nextByte() {
+        return memoryManager.read(PC++);
+    }
+
+    private void execute() {
+        int opcode = IR >>> 24;
+
+        switch (opcode) {
+            case OPCODE_END: {
+                PSW.set(PSW_INT_END);
+            }
+            break;
+            case OPCODE_ASSIGNMENT: {
+                //int operand = IR & 0x000000ff;
+                int operand = nextByte();
+                AX = operand;
+                if (AX == 0) {
+                    PSW.set(PSW_ZF);
+                }
+                if (AX <= 0) {
+                    PSW.set(PSW_SF);
+                }
+            }
+            break;
+            case OPCODE_INCREASE: {
+                AX = (AX + 1) % 0xff;
+                if (AX == 0) {
+                    PSW.set(PSW_ZF);
+                }
+            }
+            break;
+            case OPCODE_DECREASE:
+                AX = (AX - 1) % 0xff;
+                if (AX == 0) {
+                    PSW.set(PSW_ZF);
+                }
+                break;
+            case OPCODE_IO: {
+                //int deviceID = (IR & 0x00ff0000) >> 16;
+                //int usageTime = (IR & 0x0000ff00) >> 8;
+                int deviceID = nextByte();
+                int usageTime = nextByte();
+                System.out.println("IO: " + deviceID + " " + usageTime);
+            }
+            break;
+            default:
+                break;
+        }
+
+    }
+
+    private void interruptEnd() {
+
+    }
+
+    private void interruptTime() {
+        timeslice = 6;
+    }
+
+    private void interruptIO() {
+
+    }
+
+    public void run() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                CPU();
+            }
+        }, 0, 500);
+    }
+
     /**
      * @return the time
      */
-    var time: Int = 0
-        private set
-    private var timeslice = 1
-
-    fun CPU() {
-        time++
-        if (psw.get(PSW_INT_END)) {
-            interruptEnd()
-            psw.clear(PSW_INT_END)
-        }
-        if (psw.get(PSW_INT_TIME_SLICE)) {
-            interruptTime()
-            psw.clear(PSW_INT_TIME_SLICE)
-        }
-        if (psw.get(PSW_INT_IO)) {
-            interruptIO()
-            psw.clear(PSW_INT_IO)
-        }
-
-        ir = nextByte().toInt()
-        execute()
-        if (--timeslice == 0) {
-            psw.set(PSW_INT_TIME_SLICE)
-        }
-
-        onFinished()
+    public int getTime() {
+        return time;
     }
 
-    private fun onFinished() {
-        System.out.printf(" CPU stat (Current Time: %d)\n", time)
-        System.out.printf("TimeSlice = %d\n", timeslice)
-        System.out.printf("AX = %d, PC = %d, PSW = %s\n", ax, pc, psw.toString())
-        println("----------")
+    /**
+     * @return the AX
+     */
+    public int getAX() {
+        return AX;
     }
 
-    private fun nextByte(): Byte {
-        return memoryManager.read(pc++)
+    /**
+     * @return the PSW
+     */
+    public BitSet getPSW() {
+        return PSW;
     }
 
-    private fun execute() {
-        val opcode = ir.ushr(24)
-
-        when (opcode) {
-            OPCODE_END -> {
-                psw.set(PSW_INT_END)
-            }
-            OPCODE_ASSIGNMENT -> {
-                //int operand = IR & 0x000000ff;
-                val operand = nextByte().toInt()
-                ax = operand
-                if (ax == 0) {
-                    psw.set(PSW_ZF)
-                }
-                if (ax <= 0) {
-                    psw.set(PSW_SF)
-                }
-            }
-            OPCODE_INCREASE -> {
-                ax = (ax + 1) % 0xff
-                if (ax == 0) {
-                    psw.set(PSW_ZF)
-                }
-            }
-            OPCODE_DECREASE -> {
-                ax = (ax - 1) % 0xff
-                if (ax == 0) {
-                    psw.set(PSW_ZF)
-                }
-            }
-            OPCODE_IO -> {
-                //int deviceID = (IR & 0x00ff0000) >> 16;
-                //int usageTime = (IR & 0x0000ff00) >> 8;
-                val deviceID = nextByte().toInt()
-                val usageTime = nextByte().toInt()
-                println("IO: $deviceID $usageTime")
-            }
-            else -> {
-            }
-        }
-
+    /**
+     * @return the IR
+     */
+    public int getIR() {
+        return IR;
     }
 
-    private fun interruptEnd() {
-
+    /**
+     * @return the PC
+     */
+    public int getPC() {
+        return PC;
     }
 
-    private fun interruptTime() {
-        timeslice = 6
+    public void recoveryState(ProcessControlBlock PCB) {
+        this.AX = PCB.getAX();
+        this.PC = PCB.getPC();
+        this.PSW = PCB.getPSW();
     }
 
-    private fun interruptIO() {
-
+    public int getTimeSlice() {
+        return timeslice;
     }
 
-    fun run() {
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                CPU()
-            }
-        }, 0, 500)
-    }
-
-    fun recoveryState(PCB: ProcessControlBlock) {
-        this.ax = PCB.ax
-        this.pc = PCB.pc
-        this.psw = PCB.psw
-    }
-
-    interface Listener {
-        fun on()
-    }
-
-    companion object {
-
-        private val PSW_INT_END = 9
-        private val PSW_INT_TIME_SLICE = 8
-        private val PSW_INT_IO = 7
-        private val PSW_CF = 3
-        private val PSW_SF = 2
-        private val PSW_OF = 1
-        private val PSW_ZF = 0
-
-        @JvmStatic fun main(args: Array<String>) {
-            val CPU = CentralProcessingUnit()
-            CPU.run()
-        }
+    public interface Listener {
+        void on(CentralProcessingUnit cpu);
     }
 }
--- src/emuos/os/Shell.java
+++ src/emuos/os/Shell.java
@@ -4,4 +4,23 @@ package emuos.os;
  * Created by Link on 2017/7/20.
  */
 public class Shell {
+    OutputListener outputListener = null;
+
+    public Shell(OutputListener listener) {
+        outputListener = listener;
+    }
+
+    public void write(String input) {
+
+    }
+
+    private void output(String message) {
+        if (outputListener != null) {
+            outputListener.on(message);
+        }
+    }
+
+    public interface OutputListener {
+        void on(String text);
+    }
 }
