e0a2ab0765bf056be1e104992a26e10ae2d94ef1
Link <linkdoyle@163.com>
2017-08-23 10:24:38 +0800

    add NULL object

--- src/emuos/diskmanager/FilePath.java
+++ src/emuos/diskmanager/FilePath.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class FilePath {
 
     public static final String separatorChar = "/";
+    public static final FilePath NULL = new FilePath("");
     private final String path;
     private FileSystem fs = FileSystem.getFileSystem();
 
